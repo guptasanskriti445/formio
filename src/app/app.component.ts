1a8681f9c85e3dcc0f523d6db81334bdc1e14f30
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2022-11-30 15:38:29 +0530

    formio changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild, EventEmitter, HostListener } from '@angular/core';
+import { FormioRefreshValue }  from 'angular-formio';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'formio';
+export class AppComponent implements OnInit {
+  @ViewChild('json', {static: true}) jsonElement?: ElementRef| any;
+  @ViewChild('code', {static: true}) codeElement?: ElementRef;
+  @ViewChild('formio') formio:any;
+  public form: Object;
+  public refreshForm: EventEmitter<FormioRefreshValue> = new EventEmitter();
+  constructor() {
+    this.form = {components: []};
+  }
+
+  ngOnInit(): void {
+    this.form = {components: []};
+    this.refreshForm = new EventEmitter();
+  }
+  saveForm() {
+    this.jsonElement.nativeElement.innerHTML = '';
+    this.jsonElement.nativeElement.appendChild(document.createTextNode(JSON.stringify(this.formio.form, null, 4)));
+    this.refreshForm.emit({
+      property: 'form',
+      form: this.form
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { FormioModule, FormioAppConfig } from 'angular-formio';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +11,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, FormioModule
   ],
-  providers: [],
+  providers:    [{provide: FormioAppConfig, useValue: {
+    icons: 'fontawesome'
+  }} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
